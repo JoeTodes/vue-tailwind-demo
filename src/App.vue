8fac39c6e24353d14d47f1a07f32d42b3be5ec30
JoeTodes <jtwilson.home@gmail.com>
2021-11-10 15:37:47 -0700

    initil commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <h1 class="text-center text-5xl font-bold bg-red-500">{{ count }}</h1>
+    <button
+        @click="incrementCount()"
+        class="
+            ml-48
+            hover:bg-blue-300
+            bg-gray-300
+            border-2 border-black
+            rounded-full
+            px-2
+        "
+    >
+        Click Me!
+    </button>
+    <ListOfStuff class="ml-28" />
+</template>
+
+<script>
+import ListOfStuff from "./components/ListOfStuff.vue";
+
+export default {
+    data() {
+        return {
+            count: 0,
+        };
+    },
+    components: { ListOfStuff },
+    methods: {
+        incrementCount() {
+            this.count++;
+        },
+    },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/ListOfStuff.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>Stuff Component</div>
+        <a
+            v-for="(course, index) in arrayOfStuff"
+            :href="course.link"
+            :key="index"
+            ><div>
+                {{ course.name }}
+            </div>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            arrayOfStuff: [
+                { name: "photography", link: "http://www.youtube.com" },
+                { name: "math", link: "http://www.twitter.com" },
+                { name: "physics", link: "http://www.google.com" },
+                { name: "music", link: "http://www.spotify.com" },
+            ],
+        };
+    },
+};
+</script>
+
+<style>
+</style>
