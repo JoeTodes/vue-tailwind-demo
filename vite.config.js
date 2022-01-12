2ba8fe662abfead670ac2c1b69c7cc8cc9efdc8f
Joe Wilson <jtwilson.home@gmail.com>
2022-01-12 22:13:52 +0000

    added gitpod HMR fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    hmr: process.env.GITPOD_WORKSPACE_URL
+      ? {
+        // removes the protocol and replaces it with the port we're connecting to
+        host: process.env.GITPOD_WORKSPACE_URL.replace('https://', '3000-'),
+        protocol: 'wss',
+        clientPort: 443
+      }
+      : true
+  }
 })
